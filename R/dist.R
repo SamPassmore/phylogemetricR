08d4f0c04ede1b5d54cceaf32f8c80a71ecaf685
Sam Passmore <passmore.sam@gmail.com>
2023-03-28 11:01:42 +1100

    code formatting

--- R/dist.R
+++ R/dist.R
@@ -1,8 +1,9 @@
-hammingdist <- function(a, b){
+hammingdist <- function(a, b) {
   same <- 0.0
   compared <- 0.0
-  for (i in 1:nchar(a)){
-    if (substr(a, i, i) %in% c("?", "-") || substr(b, i, i) %in% c("?", "-")) {
+  for (i in 1:nchar(a)) {
+    if (substr(a, i, i) %in% c("?", "-") ||
+        substr(b, i, i) %in% c("?", "-")) {
       next
     } else if (substr(a, i, i) == substr(b, i, i)) {
       same <- same + 1.0
@@ -12,17 +13,19 @@ hammingdist <- function(a, b){
   return(1.0 - (same / compared))
 }
 
-euclideandist <- function(a, b) sqrt(sum((a - b)^2))
+euclideandist <- function(a, b)
+  sqrt(sum((a - b) ^ 2))
 
-dist.phylogemetric = function(a, b, method = "hamming"){
-  if(method == "hamming"){
+dist.phylogemetric = function(a, b, method = "hamming") {
+  if (method == "hamming") {
     a = paste(a, collapse = "")
     b = paste(b, collapse = "")
-    dd = hammingdist(a,b)
+    dd = hammingdist(a, b)
   }
 
-  if(method == "euclidean")
+  if (method == "euclidean") {
     dd = euclideandist(a, b)
+  }
 
   return(dd)
 }
